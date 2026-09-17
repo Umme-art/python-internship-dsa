51aac1c0ecded0a5bf2659b5e88a47f2611b1276
Umme <ummeasifa84@gmail.com>
2026-09-17 16:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (77.96%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+
+        candidates.sort()
+        res = []
+
+        def dfs(target, start, comb):
+            if target < 0:
+                return
+            if target == 0:
+                res.append(comb)
+                return
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i-1]:
+                    continue
+                if candidates[i] > target:
+                    break
+                dfs(target-candidates[i], i+1, comb+[candidates[i]])
+
+        dfs(target, 0, [])
+        return res  
