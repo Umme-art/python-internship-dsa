d108c5f0f85de5698f9f3729615f6706854ab969
Umme <ummeasifa84@gmail.com>
2026-09-21 11:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 31.8 MB (67.35%) - LeetSync

---
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    bool isValid(vector<vector<int>>& grid, int row, int col, int n, int expValue)
+    {
+        if (row < 0 || col < 0 || row >= n || col >= n || grid[row][col] != expValue)
+        {
+            return false;
+        }
+        if (expValue == n * n - 1)
+        {
+            return true;
+        }
+
+        bool ans1 = isValid(grid, row - 2, col + 1, n, expValue + 1);
+        bool ans2 = isValid(grid, row - 1, col + 2, n, expValue + 1);
+        bool ans3 = isValid(grid, row + 1, col + 2, n, expValue + 1);
+        bool ans4 = isValid(grid, row + 2, col + 1, n, expValue + 1);
+        bool ans5 = isValid(grid, row + 2, col - 1, n, expValue + 1);
+        bool ans6 = isValid(grid, row + 1, col - 2, n, expValue + 1);
+        bool ans7 = isValid(grid, row - 1, col - 2, n, expValue + 1);
+        bool ans8 = isValid(grid, row - 2, col - 1, n, expValue + 1);
+
+        return ans1 || ans2 || ans3 || ans4 || ans5 || ans6 || ans7 || ans8;
+    }
+
+    bool checkValidGrid(vector<vector<int>>& grid)
+    {
+        return isValid(grid, 0, 0, grid.size(), 0);
+    }
+};
